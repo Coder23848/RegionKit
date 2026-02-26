74b35f052928d393e925ffabde6f25997403bacc
alduris <152772325+alduris@users.noreply.github.com>
2026-02-25 21:48:42 -0500

    Increase initial delay of construction gate tut

--- src/Modules/Misc/ConstructionGateTutorial.cs
+++ src/Modules/Misc/ConstructionGateTutorial.cs
@@ -14,7 +14,7 @@
 			if (room.game.session is StoryGameSession && room.game.cameras[0].hud != null && room.game.Players.Count > 0 && room.game.Players[0].realizedCreature != null && room.game.Players[0].realizedCreature.room == room)
 			{
 				ExtendedGates.hasSeenConstructionGateTutorial = true;
-				room.game.cameras[0].hud.textPrompt.AddMessage(room.game.rainWorld.inGameTranslator.Translate("You do not have the region this gate connects to."), 20, 160, true, true);
+				room.game.cameras[0].hud.textPrompt.AddMessage(room.game.rainWorld.inGameTranslator.Translate("You do not have the region this gate connects to."), 40, 160, true, true);
 				if (room.game.cameras[0].hud.textPrompt.subregionTracker != null)
 				{
 					room.game.cameras[0].hud.textPrompt.subregionTracker.lastShownRegion = 1;
